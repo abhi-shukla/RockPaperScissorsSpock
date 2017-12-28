7400e27ff298965459a0cb099fbe44d7a82c2902
Abhishek Shukla <dev@abhishekshukla.com>
2017-12-28 01:12:20 -0800

    Add classes for template app

---
+++ GameApp/Controllers/MessagesController.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace GameApp
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if(activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if(message.Type == ActivityTypes.DeleteUserData)
+            {
+
+            }
+            else if(message.Type == ActivityTypes.ConversationUpdate)
+            {
+
+            }
+            else if(message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+
+            }
+            else if(message.Type == ActivityTypes.Typing)
+            {
+
+            }
+            else if(message.Type == ActivityTypes.Ping)
+            {
+
+            }
+
+            return null;
+        }
+    }
+}
---
+++ GameApp/Dialogs/RootDialog.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace GameApp.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            int length = (activity.Text ?? string.Empty).Length;
+
+            await context.PostAsync(length.ToString());
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
