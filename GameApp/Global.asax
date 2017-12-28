cf6fe366be4c28873d506504d8e137882095e527
Abhishek Shukla <dev@abhishekshukla.com>
2017-12-28 01:24:06 -0800

    Fix global asax file

--- GameApp/Global.asax
+++ GameApp/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GameApp.Global" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GameApp.WebApiApplication" Language="C#" %>
--- GameApp/Global.asax.cs
+++ GameApp/Global.asax.cs
@@ -8,42 +8,12 @@ using System.Web.SessionState;
 
 namespace GameApp
 {
-    public class Global : HttpApplication
+    public class WebApiApplication : HttpApplication
     {
 
         protected void Application_Start(object sender, EventArgs e)
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
-        }
-
-        protected void Session_Start(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_BeginRequest(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_AuthenticateRequest(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_Error(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Session_End(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_End(object sender, EventArgs e)
-        {
-
         }
     }
 }
